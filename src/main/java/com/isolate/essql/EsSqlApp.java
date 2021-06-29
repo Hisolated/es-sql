f4b81c015b15f3d4630bd534f4857f13bc469f2f
isolated <15071340963@163.com>
2021-06-29 13:39:02 +0800

    first commit

---
+++ src/main/java/com/isolate/essql/EsSqlApp.java
@@ -0,0 +1,17 @@
+package com.isolate.essql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: some desc
+ * @author: isolate
+ * @email: 15071340963@163.com
+ * @date: 2021/6/29 10:53
+ */
+@SpringBootApplication
+public class EsSqlApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EsSqlApp.class,args);
+    }
+}
